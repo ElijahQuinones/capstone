1424bb93540bde43a79401e1dc0040a24cb034b9
nikolasventouris <nrventouris@gmail.com>
2023-03-07 18:08:01 -0500

    what you changed goes here

--- table/table.js
+++ table/table.js
@@ -1,10 +1,24 @@
-$(document).ready( function () {
-    $('#table').DataTable();
-        'ajax' ; '/table.json'
-        'columns' ; [
-            {'data' : 'a'},
-            {'data' : 'b'},
-            {'data' : 'c'},
-            {'data' : 'd'}
-        ]
-} );
+$(document).ready(function () {
+    $('#example').DataTable({
+        ajax: './data.json',
+        columns: [
+            { Records: 'eventVersion' },
+            {
+                Records: {
+                    _: 'userIdentity.type',
+                    sort: 'userIdentity.principleId',
+                    sort: 'userIdentity.arn',
+                    sort: 'userIdentity.accessKeyId',
+                    sort: 'userIdentity.accountId',
+                    sort: 'userIdentity.userName',
+                },
+            },
+            { Records: 'eventTime' },
+            { Records: 'eventSource' },
+            { Records: 'eventName' },
+            { Records: 'awsRegion' },
+            { Records: 'sourceIPAddress' },
+            { Records: 'userAgent' },
+        ],
+    });
+});
