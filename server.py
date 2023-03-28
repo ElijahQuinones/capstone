e5438332f94207af2108b3e496aeb22df9e96c96
ElijahQuinones <118852979+ElijahQuinones@users.noreply.github.com>
2023-03-27 21:33:46 -0400

    Update server.py

--- server.py
+++ server.py
@@ -28,8 +28,8 @@ def register():
     return render_template("register.html")
 
 @app.route("/loggedin")
-  def login():
-           return render_template("dashboard.html")
+def login():
+    return render_template("dashboard.html")
 
 # Test route where AWS data is displayed. 
 # Event name parameter is required 
