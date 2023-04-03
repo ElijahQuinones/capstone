a07c3e49e04412ed74cab552193d07491ee81aa7
nikolasventouris <nrventouris@gmail.com>
2023-04-02 21:30:21 -0400

    added links, cleaned up styling, data now dislays on table but i still need to setup a dedicated table page

--- static/dashboard/table.js
+++ static/dashboard/table.js
@@ -1,24 +1,17 @@
-$(document).ready(function () {
+$(document).ready(function() {
     $('#example').DataTable({
-        ajax: './data.json',
-        columns: [
-            { Records: 'eventVersion' },
-            {
-                Records: {
-                    _: 'userIdentity.type',
-                    sort: 'userIdentity.principleId',
-                    sort: 'userIdentity.arn',
-                    sort: 'userIdentity.accessKeyId',
-                    sort: 'userIdentity.accountId',
-                    sort: 'userIdentity.userName',
-                },
-            },
-            { Records: 'eventTime' },
-            { Records: 'eventSource' },
-            { Records: 'eventName' },
-            { Records: 'awsRegion' },
-            { Records: 'sourceIPAddress' },
-            { Records: 'userAgent' },
-        ],
+        "responsive": true,
+        "ajax": {
+            "url": "data.json",
+            "dataSrc": "Records"
+        },
+        "columns": [
+            { "data": "userIdentity.userName" },
+            { "data": "userIdentity.accountId" },
+            { "data": "eventTime" },
+            { "data": "eventName" },
+            { "data": "awsRegion" },
+            { "data": "sourceIPAddress" }
+        ]
     });
 });
