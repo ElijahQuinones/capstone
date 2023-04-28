3c88f17c1149711492accdd75a75efab4f5c1e60
ElijahQuinones <118852979+ElijahQuinones@users.noreply.github.com>
2023-04-27 21:22:06 -0400

    cleaning up files

--- server.py
+++ server.py
@@ -1,32 +1,13 @@
-from flask import Flask,render_template,url_for,request,redirect,make_response
-import sqlite3
+from flask import Flask,render_template,request,redirect
 from waitress import serve
 from cloudtrail_helper import event_data 
 app = Flask(__name__)
 
 
-# this will be removed and  replaced with the real backend code of the project at a later date
-
-
-
-
-@app.route("/profile")
-def EJ_profile():
-    my_name_jason = {
-        "name":"Elijah Quinones",
-        "about":"I am a software engineer fellow at Hack.Diversity"
-    }
-    return my_name_jason
-
 
 @app.route("/", methods = ['GET','POST'])
 def home():
     return redirect("https://itcapstone.auth.us-east-1.amazoncognito.com/login?client_id=nni18qf04rvoq1p64edejus30&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Faws1.onrender.com%2Floggedin")
-
-@app.route("/register")
-def register():
-    
-    return render_template("register.html")
 
 @app.route("/loggedin", methods = ['GET','POST'])
 def login():
@@ -44,17 +25,6 @@ def login():
 def get_data(event_name, days):
     return event_data(event_name, days)
 
-@app.route("/metrics", methods = ["GET", "POST"])
-def metrics():
-    if  request.method == "POST":
-        event_name_input = request.form['event_name_input']
-        days_input = request.form['days_input']
-        print(input)
-        return redirect(url_for('get_data', event_name=event_name_input, 
-                                days=days_input))
-    else:
-        return render_template("get_metrics.html")
-
 @app.route("/boto3/cloudtrail/<event_name>/<int:days>")
 def get_cloudtrail_data(event_name, days):
     # limit days to 90 
@@ -67,4 +37,5 @@ def get_cloudtrail_data(event_name, days):
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port= 50100, threads =2)
+    
     
